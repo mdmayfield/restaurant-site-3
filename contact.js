f0cf1c4620d3d9bf6bb8f223cdff54df7a703872
Matt Mayfield <mdmayfield@users.noreply.github.com>
2019-04-29 20:58:45 -0500

    Use return in onsubmit so the form doesn't reset on false

--- contact.js
+++ contact.js
@@ -4,37 +4,10 @@ function validateForm() {
   var email = document.forms["contact"]["email"].value;
   var phone = document.forms["contact"]["phone"].value
 
+  console.log(name);
+  console.log(email);
+  console.log(phone);
+
   return false;
 }
-
-
-
-
-
-
-/* clearErrors();
-  var num1 = document.forms["numberFun"]["num1"].value;
-  var num2 = document.forms["numberFun"]["num2"].value;
-  if (num1 == "" || isNaN(num1)) {
-    alert("Num1 must be filled in with a number.");
-    document.forms["numberFun"]["num1"]
-    .parentElement.className = "form-group has-error";
-    document.forms["numberFun"]["num1"].focus();
-    return false;
-  }
-  if (num2 == "" || isNaN(num1)) {
-    alert("Num2 must be filled in with a number.");
-    document.forms["numberFun"]["num2"]
-    .parentElement.className = "form-group has-error";
-    document.forms["numberFun"]["num2"].focus();
-    return false;
-  }
-  document.getElementById("results").style.display = "block";
-  document.getElementById("submitButton").innerText = "Recalculate";
-  /* because + could be interpreted as concatenation instead of addition 
-  document.getElementById("addResult").innerText = Number(num1) + Number(num2);
-  document.getElementById("subtractResult").innerText = num1 - num2;
-  document.getElementById("multiplyResult").innerText = num1 * num2;
-  document.getElementById("divideResult").innerText = num1 / num2;
-  /* return false so form doesn't submit and we lose the results */
 
